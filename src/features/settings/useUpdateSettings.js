6ca949c97efe56cd36a0dcacb15438f0be3f85af
islamsaeedDev <islamsaeed891@gmail.com>
2025-11-22 23:03:56 +0200

    creating  settings of  the booking dashboard

---
+++ src/features/settings/useUpdateSettings.js
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { toast } from "react-hot-toast";
+import { updateSetting as updateSettingsAPI } from "../../services/apiSettings";
+
+export function useUpdateSettings() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateSettings, isLoading: isUpdating } = useMutation({
+    mutationFn: updateSettingsAPI,
+    onSuccess: () => {
+      toast.success("Settings successfully edited");
+      queryClient.invalidateQueries({ queryKey: ["settings"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isUpdating, updateSettings };
+}
